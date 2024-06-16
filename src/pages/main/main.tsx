cfacc991de003348556c677c75408fb2cd3ba14d
minhoshin11 <tlsalsgh777@naver.com>
2024-06-17 01:33:33 +0900

    메인페이지에 버튼이 눌려지는 오류 해결

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./index.module.css";
 
 export default function Main() {
   const navigate = useNavigate();
+  const [buttonsVisible, setButtonsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setButtonsVisible(true);
+    }, 5000);
+  }, [buttonsVisible]);
 
   return (
     <div>
@@ -21,23 +29,11 @@ export default function Main() {
         </div>
         <div
           className={`${styles.text} ${styles.myName}`}
-          style={{ animationDelay: "2.5s", animationDuration: "3s" }}
+          style={{ animationDelay: "2.3s", animationDuration: "3s" }}
         >
           FrontEnd 신민호
         </div>
-
-        {/* <div className={styles.line1}>방문해주셔서 감사합니다.</div>
-        <div className={styles.line2}>
-          저는 신입 프론트엔드 개발자{" "}
-          <span className={styles.name}>신민호</span>입니다.
-        </div>
-        <div className={styles.contentExplain}>
-          <div className={styles.line3}> Project는 제가 구현한 상세 내용</div>
-          <div className={styles.line4}>
-            About엔 저에 대한 정보가 담겨있습니다.
-          </div>
-        </div> */}
-        <div className={styles.buttonBox}>
+        <div className={buttonsVisible ? styles.buttonBox : styles.hidden}>
           <div>Go</div>
           <div>To</div>
           <div className={styles.buttonWrapper}>
